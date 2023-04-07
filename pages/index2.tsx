b44e94032e3c494a079e45fbb219023ff51564bd
Anson <ansoncheng1116@hotmail.com>
2023-04-07 18:33:33 +0800

    first commit

--- pages/index2.tsx
+++ pages/index2.tsx
@@ -4,49 +4,49 @@ import { useCallback, useMemo, useState } from "react";
 import { Button, Divider, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 
-const contractAddress = "0x980fC4F6577B876a59fF0878B422EABf7B4beE2C"
+const contractAddress = "0x062251Bcf7bD198850DDb256e4c6Ba03d30155a8";
 
 export default function Home() {
   const [address, setAddress] = useState();
   const [balance, setBalance] = useState("");
 
   const onEnteringLotterySystemBtnClick = async () => {
-    let confirmed = confirm("Are you sure you want to entering the lottery system?")
-    if (signer === null) return
+    let confirmed = confirm(
+      "Are you sure you want to enter the lottery system?"
+    );
+    if (signer === null) return;
     if (confirmed) {
       const lotteryContract = new ethers.Contract(
         contractAddress,
         abi.abi,
         signer
       );
-      
 
-
-      //we will call enter function and pass 0.1 ethers to it
+      // we will call enter function and pass 0.1 ether to it
       const transaction = await lotteryContract.enter({
         value: ethers.utils.parseEther("0.1"),
-      })
+      });
 
-      //wait the transaction to be mined
+      // wait for the transaction to be mined
       await transaction.wait();
 
-      alert("You have entered the lottery system!")
+      alert("You have entered the lottery system!");
     } else {
-      alert("You have not entered the lottery system")
+      alert("You have not entered the lottery system!");
     }
-  }
+  };
 
-  const getBalance = async () => {
-    if (provider === null) return
+  const getBalanace = async () => {
+    if (provider === null) return;
     const lotteryContract = new ethers.Contract(
       contractAddress,
       abi.abi,
       provider
-      );
-      const balanceFromContract = await lotteryContract.getBalance();
-      const decimalBalance = ethers.utils.formatEther(balanceFromContract)
-      setBalance(decimalBalance)
-  }
+    );
+    const balanceFromContract = await lotteryContract.getBalance();
+    const decimalBalance = ethers.utils.formatEther(balanceFromContract);
+    setBalance(decimalBalance);
+  };
 
   const connectToTheMetaMask = useCallback(async () => {
     // check if the browser has MetaMask installed
@@ -67,22 +67,26 @@ export default function Home() {
       (window as any).ethereum
     ).getSigner();
   }, [address]);
-  
+
   const provider = useMemo(() => {
     // only connect to the contract if the user has MetaMask installed
     if (typeof window === "undefined") return null;
     return new ethers.providers.Web3Provider((window as any).ethereum);
   }, []);
-  
-  
+
   return (
     <>
-    <h1>Home</h1>
-    <h4>Address: {address}</h4>
-    <h4>Balance: {balance}</h4>
-    <button onClick={connectToTheMetaMask}>Connect to Metamask</button>
-    <button onClick={getBalance}>Get balance</button>
-    <Button onClick={onEnteringLotterySystemBtnClick}>Enter Lottery system</Button>
+      <Typography variant="h1">Home</Typography>
+      <h4>{address}</h4>
+      <h4>Balance: {balance}</h4>
+      <Divider />
+      <Stack>
+        <Button onClick={connectToTheMetaMask}>Connect to MetaMask</Button>
+        <Button onClick={getBalanace}>Get balance</Button>
+        <Button onClick={onEnteringLotterySystemBtnClick}>
+          Enter lottery system
+        </Button>
+      </Stack>
     </>
-  )
+  );
 }
